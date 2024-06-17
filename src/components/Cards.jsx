ef7a5ef2a4095521a3fcada24b531ce5f19f7dc6
siddhant <siddhantgorle9@gmail.com>
2024-06-17 14:34:34 +0530

    Card Changes

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,23 +2,25 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function Cards({ item }) {
-  //const navigate = useNavigate();
+  // const navigate = useNavigate();
   return (
     <>
-      <div className="mt-4 my-3 p-3" /* onClick={() => navigate(`/cards/${item.id}`)} */>
-        <div className="card w-92 bg-base-100 shadow-xl hover:scale-105 duration-200 dark:bg-zinc-700 dark:text-white flex flex-col">
-          <figure>
-            <img src={item.image} alt="Image" />
+      <div className="mt-4 my-3 p-3">
+        <div className="card w-full bg-base-100 shadow-xl hover:scale-105 duration-200 dark:bg-zinc-700 dark:text-white flex flex-col">
+          <figure className="w-full h-96">
+            <img src={item.image} alt="Image" className="w-full h-full object-cover" />
           </figure>
-          <div className="card-body max-h-full">
-            <h2 className="card-title h-1 contents">
-              {item.name}
-              <div className="badge badge-secondary">{item.category}</div>
-            </h2>
-            <p>{item.title}</p>
-            <div className="card-actions justify-between">
+          <div className="card-body flex flex-col justify-between p-4 h-1/2">
+            <div>
+              <h2 className="card-title text-lg">
+                {item.name}
+                <div className="badge badge-secondary ml-2">{item.category}</div>
+              </h2>
+              <p className="text-sm">{item.title}</p>
+            </div>
+            <div className="card-actions flex justify-between items-center mt-4">
               <div className="badge badge-outline">${item.price}</div>
-              <div className=" cursor-pointer px-2 py-1 rounded-full border-[2px] hover:bg-pink-500 hover:text-white duration-200">
+              <div className="cursor-pointer px-2 py-1 rounded-full border-2 hover:bg-pink-500 hover:text-white duration-200">
                 Buy Now
               </div>
             </div>
